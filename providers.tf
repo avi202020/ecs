1cc46042c872b81dc4266dce2322d5f65669f619
avi202020 <53336818+avi202020@users.noreply.github.com>
2020-01-07 10:08:11 +0000

    Add files via upload
    commit

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "javahome-tf-12345678"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "javahome-tf"
+  }
 }
